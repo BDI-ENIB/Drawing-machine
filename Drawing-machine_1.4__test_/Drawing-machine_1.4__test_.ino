46f1783b4227998bf402e61ce8f14d972631092f
Raphael-Nyphalem <Raphael-Nyphalem@users.noreply.github.com>
2018-09-21 12:11:37 +0200

    formatage automatique

--- Drawing-machine_1.4__test_/Drawing-machine_1.4__test_.ino
+++ Drawing-machine_1.4__test_/Drawing-machine_1.4__test_.ino
@@ -9,20 +9,20 @@ Servo servo;
 
 
 /*
-x ou y ou z represente l'axe de deplacement
-le chiffre represente l'angle de rotation
-$ est le code pour fermer la boucle 
+  x ou y ou z represente l'axe de deplacement
+  le chiffre represente l'angle de rotation
+  $ est le code pour fermer la boucle
 
 
-Example de commande de demande:
-x90$ y70$ z90$  x-80$ y-70$
+  Example de commande de demande:
+  x90$ y70$ z90$  x-80$ y-70$
 
 
 
 */
 
 
-//Moteur pas a pas config 
+//Moteur pas a pas config
 
 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
 #define MOTOR_STEPS 200
@@ -48,14 +48,14 @@ int pastY = 0;
 boolean posZ = false;
 
 // comunication
-int x =0;
-int tmp_x =0;
+int x = 0;
+int tmp_x = 0;
 
-int y =0;
-int tmp_y =0;
+int y = 0;
+int tmp_y = 0;
 
-int z =0;
-int tmp_z =0;
+int z = 0;
+int tmp_z = 0;
 
 // Servo motor
 int ser;
@@ -67,139 +67,139 @@ int bp_y = 1;
 int entbp_x = 12;
 int entbp_y = 13;
 
-void reset(){  
+void reset() {
 
   servo.write(90);
-    
-while(bp_x == 1){
-      stepperx.move(-4);
-      bp_x = digitalRead(entbp_x);
-   }
-    pastX=0;
-    
-    while(bp_y == 1){
-      steppery.move(-4);
-      bp_y = digitalRead(entbp_y);
-   }
-    pastY=0;
-  
+
+  while (bp_x == 1) {
+    stepperx.move(-4);
+    bp_x = digitalRead(entbp_x);
+  }
+  pastX = 0;
+
+  while (bp_y == 1) {
+    steppery.move(-4);
+    bp_y = digitalRead(entbp_y);
+  }
+  pastY = 0;
+
 }
 
 
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-   //setup motor
-    stepperx.begin(RPM, MICROSTEPS);
-    steppery.begin(RPM, MICROSTEPS);
+  //setup motor
+  stepperx.begin(RPM, MICROSTEPS);
+  steppery.begin(RPM, MICROSTEPS);
 
-   //servo motor
-    servo.attach(attache);
+  //servo motor
+  servo.attach(attache);
 
-    pinMode(entbp_x, INPUT_PULLUP);
-    pinMode(entbp_y, INPUT_PULLUP);
+  pinMode(entbp_x, INPUT_PULLUP);
+  pinMode(entbp_y, INPUT_PULLUP);
 
-    reset();
+  reset();
 
-   //Start
-    Serial.print("Demar");
-    Serial.println("rage");
+  //Start
+  Serial.print("Demar");
+  Serial.println("rage");
 
 }
 void loop() {
 
-detection();
-if (posX != 0 or posY != 0 or posZ == true){
-  positions();
-  deplacement();
-}
-}
-void positions(){
-  
-  x = pastX-posX;
-  y = pastY-posY;
-
-
-    pastX = posX;
-    pastY = posY;
-
- posX = 0;
- posY = 0;
- posZ = false;
-  
-  
+  detection();
+  if (posX != 0 or posY != 0 or posZ == true) {
+    positions();
+    deplacement();
   }
-void deplacement(){
+}
+void positions() {
+
+  x = pastX - posX;
+  y = pastY - posY;
+
+
+  pastX = posX;
+  pastY = posY;
+
+  posX = 0;
+  posY = 0;
+  posZ = false;
+
+
+}
+void deplacement() {
 
   servo.write(z);
-  
-  controller.move(x,y);
-  
-  
-  }
-void detection(){
+
+  controller.move(x, y);
+
+
+}
+void detection() {
   ser = Serial.read();
- 
+
   if ( ser > 0) {
-      switch(ser){                          //Dection des entres du serial
+    switch (ser) {                        //Dection des entres du serial
 
-        case 120:                   //pour detecter x 
-          while (ser != 32){
-            ser = Serial.read();
-            if ( ser == 32){       //Si le signe " "(espace) (signe de fin de commande) est donner 
-              break;
-            }
-              
-            if ( ser >= 0) {        //Memoire du nombre
-              tmp_x = (tmp_x * 10) + (ser-48);                     
-            }
-          }
-          
-          posX = tmp_x;
-          tmp_x = 0;
-          break;
-
-
-        
-        case 121:                 //pour detecter y
-          while (ser != 32){
-            ser = Serial.read();
-            
-            if ( ser == 32){      //Si les signe " "(espace) (signe de fin de commande) est donner
-              break;
-            }
-            
-            
-            if ( ser >= 0) {      //Memoire du nombre
-              tmp_y = (tmp_y * 10) + (ser-48);                    
-            }
-          }
-          
-
-          posY = tmp_y;
-          tmp_y = 0;
-          
-          break;
-
-
-          
-          case 122:  //pour detecter z 
-          while (ser != 32){
-            ser = Serial.read();
-            posZ = true;
-            if ( ser == 32){      //Si le signe " "(espace) (signe de fin de commande) est donner
-              break;
-            }
-            
-            if ( ser >= 0) {
-            tmp_z = (tmp_z * 10) + (ser-48); 
-            }
-            z=tmp_z;
-            tmp_z=0;
-            
+      case 120:                   //pour detecter x
+        while (ser != 32) {
+          ser = Serial.read();
+          if ( ser == 32) {      //Si le signe " "(espace) (signe de fin de commande) est donner
             break;
           }
-      }
+
+          if ( ser >= 0) {        //Memoire du nombre
+            tmp_x = (tmp_x * 10) + (ser - 48);
+          }
+        }
+
+        posX = tmp_x;
+        tmp_x = 0;
+        break;
+
+
+
+      case 121:                 //pour detecter y
+        while (ser != 32) {
+          ser = Serial.read();
+
+          if ( ser == 32) {     //Si les signe " "(espace) (signe de fin de commande) est donner
+            break;
+          }
+
+
+          if ( ser >= 0) {      //Memoire du nombre
+            tmp_y = (tmp_y * 10) + (ser - 48);
+          }
+        }
+
+
+        posY = tmp_y;
+        tmp_y = 0;
+
+        break;
+
+
+
+      case 122:  //pour detecter z
+        while (ser != 32) {
+          ser = Serial.read();
+          posZ = true;
+          if ( ser == 32) {     //Si le signe " "(espace) (signe de fin de commande) est donner
+            break;
+          }
+
+          if ( ser >= 0) {
+            tmp_z = (tmp_z * 10) + (ser - 48);
+          }
+          z = tmp_z;
+          tmp_z = 0;
+
+          break;
+        }
+    }
   }
 }
 
